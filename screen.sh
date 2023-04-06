65f0b18b9ec6ff3bb7eb6ee964decebb60067a31
Adnaan Hussain <adnaan@live.com>
2023-04-06 03:47:48 +0100

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,31 +1,39 @@
 #! /bin/bash
+file="$1";shift
+echo $file
+readarray -t lines < file
+
+pf(){
+	tput clear
+	for (( i=0;i<="${#lines[@]}";i++ ));do
+		selected=false
+		for input in "${@}";do [[ $i -eq $input ]] && { selected=true;break; }; done
+		if [[ $selected == true ]];then
+			echo -e "[44m${line}[0m"
+		else
+			echo "$line"
+		fi
+	done
+}
+
+
+
 
 onExit(){
 	echo -e "[?1000l";
 	tput rmcup;
 }
-tput smcup
 trap 'onExit' EXIT
-cutRow(){
-	line="$1"
-	tput sc  # save the current cursor position
-	tput cup ${line} 0  # move the cursor to line x and column 0
-	tput el  # clear the line from the cursor position to the end of the line
-	#tput ed  # clear the screen from the cursor position to the end of the screen
-	#tput cup ${line} 0  # move the cursor back to line x and column 0
-	tput rmcup  # restore the saved cursor position
-}
+tput smcup
 
 pasteRow(){
 	tput sc  # save the current cursor position
 	tput cup x 0  # move the cursor to line x and column 0
 	pbpaste | tr -d '\n' | tput el1  # retrieve clipboard contents, remove newlines, and paste into line
-	tput rmcup  # restore the saved cursor position
+	tput rc  # restore the saved cursor position
 }
 
-
-rows=$(tput lines)  # get the number of rows of the screen
-oldrows=${rows}
+oldrows=0
 hl(){
 	echo -en "[43m"
 	pasteRow
@@ -36,20 +44,20 @@ cl(){
 	tput cup $((rows-8)) 0
 	tput ed
 }
-
 draw(){
 	tput clear  # clear the screen
 	for ((i=1; i<=$((rows-8)); i++)); do
 		echo "This is line $i"
 	done
 }
-draw
+cuttxt=""
 echo mouse enabled
 
 #tput smmous  # enable mouse reporting
 echo -e "\e[?1015h"
 echo -e "\e[?1000h"
-
+row=-1
+tput sc
 while true; do
 	rows=$(tput lines)  # get the number of rows of the screen
 	[[ $oldrows -ne $rows ]] && { draw; }
@@ -60,37 +68,15 @@ while true; do
 		if [[ $ma =~ ^\[[0-9]+\;[0-9]+\;[0-9]+M ]]; then  # if the characters are a valid mouse event
 			code=$(echo $ma | cut -d ';' -f 1)
 			col=$(echo $ma | cut -d ';' -f 2)
-			row=$(echo $ma | cut -d ';' -f 3)
-			row=${row%M}
+			row=$(echo $ma | cut -d ';' -f 3); row=${row%M}
 			[[ $code == "[32" ]] && { cl; event="Started"; } || [[ $code == "[35" ]] && event="Ended" || event="Unknown"
 			echo
-			[[ -n col ]] && echo -n "Selection $event :Column $col"	
-			[[ -n row ]] && echo "  -  Selection $event : ROW $row"	
-				echo -e $ma
-
-			#sleep 1
-			#cutRow $row
-			#hl
+			[[ -n row ]] && echo -n "  -  Selection $event : ROW $row"	
+			[[ -n col ]] && echo "  -  Selection $event :Column $col"	
+			#tput sc; tput cup $row 0; cuttxt="$(tput el)"; tput rc;	
+			#echo c- $cuttxt
 		fi
 	fi
 
 
 done
-
-
-readmouse(){
-	read -rsn1 -t 0.1 key  # read a key with a timeout of 0.1 second
-	echo -n $key
-	if [[ $key = $'\e' ]]; then  # if the key is escape (the start of an escape sequence)
-		read -rsn2 # read the next two characters (the mouse event)
-	echo -e " mouse event "$REPLY
-		if [[ $REPLY =~ ^\[[0-9]+\;[0-9]+\;[0-9]+ ]]; then  # if the characters are a valid mouse event
-			row=$(echo $REPLY | cut -d ';' -f 2)  # get the row of the mouse click
-			# highlight the line here
-			echo -en "[43m"
-			sleep 2
-			echo -en "[0m"
-		fi
-	fi
-
-}
